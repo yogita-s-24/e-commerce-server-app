e1cf38a358a7301df557ab88d01db54e5ac2b5da
yogita-s-24 <yogitashete24@gmail.com>
2023-10-20 21:31:19 +0530

    mongodb connected

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 
+const MONGODB_URI = process.env.MONGODB_URI;
+
+const connectMongoDB = async () => {
+  const conn = await mongoose.connect(MONGODB_URI);
+
+  if (conn) {
+    console.log("Connected to MongoDB Successfully...");
+  }
+};
+connectMongoDB();
+
+app.get("/health", (req, res) => {
+  res.send("Status : Ok");
+});
+
+const PORT = 3000;
 
 
-app.get("/health", (req,res)=>{
-    res.send("Status : Ok");
-})
 
-
-
-const PORT = 5000;
-
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
